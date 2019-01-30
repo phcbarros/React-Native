2141143c8ce864fbe9db0a41faa7d24a49a2fb6d
Paulo Barros <paulobarros@Paulos-MacBook-Pro.local>
2019-01-30 13:40:52 -0200

    Curso React-Native - Aula 20 - Criando Estilo

--- curso-react-native/exercicios/src/componentes/simples.js
+++ curso-react-native/exercicios/src/componentes/simples.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Padrao from '../estilos/padrao'
 
 export default props => (
-    <Text>{ props.texto }</Text>
+	<Text style={Padrao.ex}>{props.texto}</Text>
 )
