d43a44d2d9faaa624a691b1beba5a2c3bf5bbf1f
Paulo Barros <paulobarros@Paulos-MacBook-Pro.local>
2019-02-04 10:38:02 -0200

    Curso React-Native - Aula 34 - Lista De Alunos #02

--- curso-react-native/exercicios/src/componentes/listaFlex.js
+++ curso-react-native/exercicios/src/componentes/listaFlex.js
@@ -5,7 +5,7 @@ const alunos = [
   { id: 1, nome: 'Paulo', nota: 10 },
   { id: 2, nome: 'Maria', nota: 0 },
   { id: 3, nome: 'Lucas', nota: 8.1 },
-  { id: 4, nome: 'Jona', nota: 9.9 },
+  { id: 4, nome: 'Joana', nota: 9.9 },
   { id: 5, nome: 'Lucia', nota: 4.3 },
   { id: 6, nome: 'Simone', nota: 2.0 },
   { id: 7, nome: 'Diego', nota: 1.2 },
@@ -15,7 +15,7 @@ const alunos = [
   { id: 11, nome: 'Paulo', nota: 10 },
   { id: 12, nome: 'Maria', nota: 0 },
   { id: 13, nome: 'Lucas', nota: 8.1 },
-  { id: 14, nome: 'Jona', nota: 9.9 },
+  { id: 14, nome: 'Joana', nota: 9.9 },
   { id: 15, nome: 'Lucia', nota: 4.3 },
   { id: 16, nome: 'Simone', nota: 2.0 },
   { id: 17, nome: 'Diego', nota: 1.2 },
@@ -28,11 +28,23 @@ const itemEstilo = {
   height: 50,
   backgroundColor: '#ddd',
   borderWidth: 1.5,
-  borderColor: '#222'
+  borderColor: '#222',
+
+  //Flex main-axis default column
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between'
+
 }
 
 export const Aluno = props => 
   <View style={itemEstilo}>
+    { /* 
+      JustifyContent aplicado no main-axis que nesse caso foi alterado para a linha,
+      e como o seu valor é `space-between` os Texts ficam separados. 
+      AlignItens é aplicado no cross-axis que nesse caso é a coluna, 
+      e como seu valor é `center` os Texts ficam alinhados no centro do componente View
+    */}
     <Text>{props.nome}</Text>
     <Text style={{fontWeight: 'bold'}}>{props.nota}</Text>
   </View>
