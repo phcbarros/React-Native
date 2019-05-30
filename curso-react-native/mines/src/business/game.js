766f6d2f716e46c8611ef7f4543b65bb6c2ab856
Paulo Barrose <paulo.barros@lambda3.com.br>
2019-05-30 12:41:15 -0300

    Curso React-Native - Campo Minado - 53 - Logica do jogo #01

---
+++ curso-react-native/mines/src/business/game.js
@@ -0,0 +1,43 @@
+const createBoard = (rows, cols) => {
+  return Array(rows)
+    .fill(0)
+    .map((_, row) => {
+      return Array(cols)
+        .fill(0)
+        .map((_, col) => {
+          return {
+            row,
+            col,
+            mined: false,
+            opened: false,
+            flagged: false,
+            exploded: false,
+            nearMines: 0,
+          }
+        })
+    })
+}
+
+const spreadMines = (board, minesAmount) => {
+  const rows = board.length
+  const columns = board[0].length
+  let minesPlanted = 0
+
+  while (minesPlanted < minesAmount) {
+    const rowSel = parseInt(Math.random() * rows, 10)
+    const columnSel = parseInt(Math.random() * columns, 10)
+
+    if (!board[rowSel][columnSel].mined) {
+      board[rowSel][columnSel].mined = true
+      minesPlanted++
+    }
+  }
+}
+
+const createMineBoard = (rows, columns, minesAmount) => {
+  const board = createBoard(rows, columns)
+  spreadMines(board, minesAmount)
+  return board
+}
+
+export { createMineBoard }
