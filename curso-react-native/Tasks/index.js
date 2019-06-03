4354f92506decdd9b45b35ff0a6516a2a4815620
Paulo Barrose <paulo.barros@lambda3.com.br>
2019-06-03 12:58:23 -0300

    Curso React-Native - Tasks - 64 - Componente Agenda

--- curso-react-native/Tasks/index.js
+++ curso-react-native/Tasks/index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import Agenda from "./src/screens/agenda/Agenda";
+import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Agenda);
