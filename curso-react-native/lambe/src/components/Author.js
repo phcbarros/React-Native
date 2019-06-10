7f39d561cedde3ed3e6c0d310b95f6127facee5c
Paulo Barrose <paulo.barros@lambda3.com.br>
2019-06-10 13:17:41 -0300

    Curso React-Native - Clone Instagram - 104 - Componente Autor

---
+++ curso-react-native/lambe/src/components/Author.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import { Gravatar } from 'react-native-gravatar'
+
+export default (props) => {
+  return (
+    <View style={styles.container}>
+      <Gravatar
+        style={styles.avatar}
+        options={{
+          email: props.email,
+          secure: true,
+        }}
+      />
+      <Text style={styles.nickname}>{props.nickname}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    marginHorizontal: 15,
+  },
+  nickname: {
+    color: '#444',
+    marginVertical: 10,
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+})
--- curso-react-native/lambe/src/components/Post.js
+++ curso-react-native/lambe/src/components/Post.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { StyleSheet, View, Image, Dimensions } from 'react-native'
+import Author from './Author'
 
 class Post extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Image source={this.props.image} style={styles.image} />
+        <Author email="phcbcontato@outlook.com" nickaname="PH" />
       </View>
     )
   }
