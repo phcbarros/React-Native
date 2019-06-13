2507d07bd02971a633cdc1f5c92bbe9a4808da6c
Paulo Barrose <paulo.barros@lambda3.com.br>
2019-06-13 14:02:39 -0300

    Curso React-Native - Clone Instagram - 117 - Login com Redux #2

--- curso-react-native/lambe/index.js
+++ curso-react-native/lambe/index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
-
+import React from 'react'
 import { AppRegistry } from 'react-native'
 import Navigator from './src/navigator/Navigator'
 import { name as appName } from './app.json'
+import { Provider } from 'react-redux'
+import storeConfig from './src/store/store-config'
 
-AppRegistry.registerComponent(appName, () => Navigator)
+const store = storeConfig()
+const Redux = () => {
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  )
+}
+
+AppRegistry.registerComponent(appName, () => Redux)
