6e2e682248374b65ab992353974e2fe57cafad2a
Paulo Barrose <paulo.barros@lambda3.com.br>
2019-06-15 10:14:32 -0300

    Curso React-Native - Clone Instagram - 128 - Usando Firebase Storage #3

--- curso-react-native/lambe/functions/index.js
+++ curso-react-native/lambe/functions/index.js
@@ -2,15 +2,50 @@ const functions = require('firebase-functions')
 const cors = require('cors')({ origin: true })
 const fs = require('fs')
 const uuid = require('uuid-v4')
-const { Storage } = require('@goggle-cloud/storage')
+const { Storage } = require('@google-cloud/storage')
 const storage = new Storage({
   projectId: 'lambe-99df9',
   keyFileName: 'lambe-99df9.json',
 })
 
+const IMAGE_PATH = '/tmp/imageToSave.jpg'
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.uploadImage = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {})
+  cors(request, response, () => {
+    try {
+      fs.writeFileSync(IMAGE_PATH, request.body.image, 'base64')
+      const bucket = storage.bucket('lambe-99df9.appspot.com')
+      const id = uuid()
+      bucket.upload(
+        IMAGE_PATH,
+        {
+          uploadType: 'media',
+          destination: `/posts/${id}.jpg`,
+          metadata: {
+            metadata: {
+              contentType: 'image/jpeg',
+              firebaseStorageDownloadTokens: id,
+            },
+          },
+        },
+        (error, file) => {
+          if (error) {
+            console.log(error)
+            return response.status(500).json({ error })
+          }
+
+          const fileName = encodeURI(file.name)
+          const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
+            bucket.name
+          }/o/${fileName}?alt=media&token=${id}`
+          return response.status(201).json({ imageUrl })
+        },
+      )
+    } catch (error) {
+      console.log(error)
+      return response.status(500).json({ error })
+    }
+  })
 })
