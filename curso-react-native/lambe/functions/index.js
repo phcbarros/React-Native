b0193ffad8b310e90662a86c70e493f507cf4e26
Paulo Barrose <paulo.barros@lambda3.com.br>
2019-06-15 09:40:00 -0300

    Curso React-Native - Clone Instagram - 127 - Usando Firebase Storage #2

--- curso-react-native/lambe/functions/index.js
+++ curso-react-native/lambe/functions/index.js
@@ -1,8 +1,16 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const cors = require('cors')({ origin: true })
+const fs = require('fs')
+const uuid = require('uuid-v4')
+const { Storage } = require('@goggle-cloud/storage')
+const storage = new Storage({
+  projectId: 'lambe-99df9',
+  keyFileName: 'lambe-99df9.json',
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.uploadImage = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {})
+})
