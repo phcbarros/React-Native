9397aa3761823d117db915ba82c126e04ca9ceea
phcbarros <paulo.barros@outlook.com.br>
2020-08-08 11:57:35 -0300

    Paper - Instalando Paper e VectorIcons

--- PaperApp/.prettierrc.js
+++ PaperApp/.prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  semi: false,
+  useTabs: false,
 };
--- PaperApp/index.js
+++ PaperApp/index.js
@@ -2,8 +2,20 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import { Provider as PaperProvider } from 'react-native-paper'
+import App from './App'
+import { name as appName } from './app.json'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-AppRegistry.registerComponent(appName, () => App);
+Icon.load()
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  )
+}
+
+AppRegistry.registerComponent(appName, () => Main)
