223357262cec16adce1cefffe486736c916c1283
phcbarros <paulo.barros@outlook.com.br>
2021-04-16 19:21:30 -0300

    Curso React Native - 7 Cadastro Completo com Hooks e Context API - 98 Criando Arquivo de usuários

---
+++ curso-react-native/rncrud/src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: '1',
+    name: 'Paulo Barros',
+    email: 'paulo.barros@email.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png',
+  },
+  {
+    id: '2',
+    name: 'Elisa Barros',
+    email: 'elisa.barros@email.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027367_1280.png',
+  },
+  {
+    id: '3',
+    name: 'Ian Miller Silva',
+    email: 'ian.silva@email.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png',
+  },
+  {
+    id: '4',
+    name: 'Ana Costa',
+    email: 'ana.costa@email.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027367_1280.png',
+  },
+]
