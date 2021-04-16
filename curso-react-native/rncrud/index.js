656860622c4bd4cd2a64127294de5509c1e211a6
phcbarros <paulo.barros@outlook.com.br>
2021-04-16 19:14:09 -0300

    Curso React Native - 7 Cadastro Completo com Hooks e Context API - 97 Navegação Stack #02

--- curso-react-native/rncrud/index.js
+++ curso-react-native/rncrud/index.js
@@ -6,7 +6,7 @@ import 'react-native-gesture-handler'
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 Icon.loadFont()
 
--- curso-react-native/rncrud/src/navegacao/Stack.js
+++ curso-react-native/rncrud/src/navegacao/Stack.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { Button, Icon } from 'react-native-elements'
 import UserForm from '../views/UserForm'
 import UserList from '../views/UserList'
 
@@ -9,10 +10,47 @@ const Stack = createStackNavigator()
 export default (props) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={'UserList'}>
-        <Stack.Screen name="UserList" component={UserList} />
-        <Stack.Screen name="UserForm" component={UserForm} />
+      <Stack.Navigator
+        initialRouteName={'UserList'}
+        screenOptions={screenOptions}>
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({ navigation }) => {
+            return {
+              title: 'Lista de Usuários',
+              headerRight: () => <AddButton {...navigation} />,
+            }
+          }}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title: 'Formulário de Usuários',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
+}
+
+const AddButton = (props) => {
+  return (
+    <Button
+      type="clear"
+      icon={<Icon name="add" size={25} color="#fff" />}
+      onPress={() => props.navigate('UserForm')}
+    />
+  )
+}
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
 }
