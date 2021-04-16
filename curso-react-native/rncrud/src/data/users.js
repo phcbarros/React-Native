44dad8db6082edb7452239006db5c3d69c4a9821
phcbarros <paulo.barros@outlook.com.br>
2021-04-16 19:53:56 -0300

    Curso React Native - 7 Cadastro Completo com Hooks e Context API - 99 Lista de Usuario #01

--- curso-react-native/rncrud/src/data/users.js
+++ curso-react-native/rncrud/src/data/users.js
@@ -11,14 +11,14 @@ export default [
     name: 'Elisa Barros',
     email: 'elisa.barros@email.com',
     avatarUrl:
-      'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027367_1280.png',
+      'https://cdn.pixabay.com/photo/2013/07/13/12/06/woman-159169_1280.png',
   },
   {
     id: '3',
     name: 'Ian Miller Silva',
     email: 'ian.silva@email.com',
     avatarUrl:
-      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png',
+      'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_1280.png',
   },
   {
     id: '4',
--- curso-react-native/rncrud/src/views/UserList.js
+++ curso-react-native/rncrud/src/views/UserList.js
@@ -1,10 +1,37 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { Avatar, ListItem } from 'react-native-elements'
+import users from '../data/users'
 
-export default () => {
+export default (props) => {
+  const renderItems = ({ item: user }) => {
+    return (
+      <ListItem
+        key={user.id}
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm')}>
+        <Avatar source={{ uri: user.avatarUrl }} avatarStyle={style.avatar} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    )
+  }
+
   return (
     <View>
-      <Text>UserList</Text>
+      <FlatList
+        data={users}
+        keyExtractor={(user) => `${user.id}`}
+        renderItem={renderItems}
+      />
     </View>
   )
 }
+
+const style = StyleSheet.create({
+  avatar: {
+    borderRadius: 25,
+  },
+})
