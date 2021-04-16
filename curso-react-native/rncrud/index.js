9eb9cd8c15c8fd9ad3cb67dec950a6cc1dd9eb25
phcbarros <paulo.barros@outlook.com.br>
2021-04-16 18:27:08 -0300

    Curso React Native - 7 Cadastro Completo com Hooks e Context API - 95 Criar Projeto

---
+++ curso-react-native/rncrud/index.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import 'react-native-gesture-handler'
+import { AppRegistry } from 'react-native'
+import App from './App'
+import { name as appName } from './app.json'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+Icon.loadFont()
+
+AppRegistry.registerComponent(appName, () => App)
