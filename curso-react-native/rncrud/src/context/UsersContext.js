c83db8d860ff4ce48cc3f589d86bd8fe3733a4e0
phcbarros <paulo.barros@outlook.com.br>
2021-04-19 17:44:15 -0300

    Curso React Native - 7 Cadastro Completo com Hooks e Context API - 106 useReducer #02

--- curso-react-native/rncrud/src/context/UsersContext.js
+++ curso-react-native/rncrud/src/context/UsersContext.js
@@ -4,9 +4,19 @@ import users from '../data/users'
 const initialState = { users }
 const UsersContext = createContext({})
 
+const actions = {
+  deleteUser(state, action) {
+    const user = action.payload
+    return {
+      ...state,
+      users: state.users.filter((u) => u.id !== user.id),
+    }
+  },
+}
 export const UsersProvider = (props) => {
   function reducer(state, action) {
-    return state
+    const fn = actions[action.type]
+    return fn ? fn(state, action) : state
   }
 
   const [state, dispatch] = useReducer(reducer, initialState)
