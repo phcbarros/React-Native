bfcbcc71dd78b9a55ed39db9b2c13b8fbd99b54c
phcbarros <paulo.barros@outlook.com.br>
2021-04-19 16:46:42 -0300

    Curso React Native - 7 Cadastro Completo com Hooks e Context API - 102 Formulário de Usuário

--- curso-react-native/rncrud/src/views/UserForm.js
+++ curso-react-native/rncrud/src/views/UserForm.js
@@ -1,12 +1,51 @@
 import React, { useState } from 'react'
-import { View, Text } from 'react-native'
+import { Button } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Input } from 'react-native-elements'
 
 export default ({ route, navigation }) => {
   const [user, setUser] = useState(route.params ? route.params : {})
 
   return (
-    <View>
-      <Text>{user.name}</Text>
+    <View style={styles.form}>
+      <Input
+        label="Name"
+        style={null}
+        leftIcon={{ name: 'account-circle' }}
+        onChangeText={(name) => setUser((prev) => ({ ...prev, name }))}
+        placeholder="Informe o nome"
+        value={user.name}
+      />
+      <Input
+        label="E-mail"
+        leftIcon={{ name: 'email' }}
+        onChangeText={(email) => setUser((prev) => ({ ...prev, email }))}
+        placeholder="Informe o e-mail"
+        value={user.email}
+      />
+      <Input
+        label="URL do Avatar"
+        leftIcon={{ name: 'camera' }}
+        onChangeText={(avatarUrl) =>
+          setUser((prev) => ({ ...prev, avatarUrl }))
+        }
+        placeholder="Informe a url do Avatar"
+        value={user.avatarUrl}
+        multiline={true}
+      />
+      <Button title="Salvar" onPress={() => navigation.goBack()} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+})
