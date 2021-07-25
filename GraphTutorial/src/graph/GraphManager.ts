3eae66f224ec548b585bafbde130364581527db8
phcbarros <paulo.barros@outlook.com.br>
2021-07-25 13:15:16 -0300

    Exemplo Microsoft Graph - Get a calendar view

--- GraphTutorial/src/graph/GraphManager.ts
+++ GraphTutorial/src/graph/GraphManager.ts
@@ -18,4 +18,24 @@ export class GraphManager {
       .select('displayName,givenName,mail,mailboxSettings,userPrincipalName')
       .get()
   }
+
+  static getCalendarView = async (
+    start: string,
+    end: string,
+    timezone: string,
+  ) => {
+    // GET /me/calendarview
+    return await graphClient
+      .api('/me/calendarview')
+      .header('Prefer', `outlook.timezone="${timezone}"`)
+      .query({startDateTime: start, endDateTime: end})
+      // $select='subject,organizer,start,end'
+      // Only return these fields in results
+      .select('subject,organizer,start,end')
+      // $orderby=createdDateTime DESC
+      // Sort results by when they were created, newest first
+      .orderby('start/dateTime')
+      .top(50)
+      .get()
+  }
 }
